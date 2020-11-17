02066761e7b13297a197304eaf1a148501589f16
Taylor Monegan <tmonegan24@biomedscienceacademy.org>
2020-11-17 14:23:18 +0000

    first draft

--- main.py
+++ main.py
@@ -4,4 +4,14 @@ Author:
 Period/Core:
 
 
-"""
+""" 
+print("lets make silly sentences")
+name = input("please enter a name: ")
+place = input("please enter a place: ")
+resteraunt = input("please enter your favorite place to eat: ")
+favorite = input("please input your favorite thing to do: ")
+food = input ("please input a food: ")
+board_game = input("please enter a board game: ")
+pet = input("please enter an animal: ")
+verb = input("please enter a verb: ")
+print(f"My name is {name}. I like to go to the {place}. My favorite thing to do at the {place} is {favorite}. \nthen I went to my favorite place to eat, {resteraunt}.I get my favorite menu item there. I get {food}. \nthen I go see my family. my favorite thing to do with my family is play board games. my favorite board game to play is {board_game}.\n finally I go to see the family {pet}. I like to {verb} with the {pet}. ")
